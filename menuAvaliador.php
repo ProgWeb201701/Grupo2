d203a299b75418620ae354ccb7423b8ebe0a11c0
Unknown <guilthys@gmail.com>
2017-06-21 16:43:16 -0300

    Menus inicais de cada usuario

--- menuAvaliador.php
+++ menuAvaliador.php
@@ -10,6 +10,47 @@
   <link rel="stylesheet" type="text/css" href="Estilo.css">
 </head>
 <body>
+<header>
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Web Tcc</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li class="dropdown">
+						<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Perfil <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="">Editar</a></li>
+								<li><a href="">Remover</a></li>
+								</ul>
+						</li>
+						<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">  Avaliações  <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href=""> Adicionar </a></li>
+								<li><a href=""> Editar Ultimo </a></li>
+								<li><a href=""> Remover </a></li>
+								</ul>
+						</li>
+				
+						<li><a href="">Cronograma</a></li>
+						<li><a href="">Ajuda</a></li>
+						
+					<li ><a href=""> Sair</a></li>
+					</ul>
+
+				</div><!--/.nav-collapse -->
+			</div>
+		</nav>
+	</header>
+
 
 </body>
 </html>
