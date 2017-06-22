ee1bf59234b2fd5e2a3e0c10c97205c19c0c7338
Vitor Hugo Maciel dos Santos <vitao375@hotmail.com>
2017-06-22 19:56:50 -0300

    Atualizzação do Banco e refatoração de Classes Model

--- Controler/Aluno/InsertAluno.php
+++ Controler/Aluno/InsertAluno.php
@@ -3,13 +3,16 @@
 ini_set('display_errors', 1);
 	$nome = $_POST['nome'];
 	$matricula = $_POST['matricula'];
+	$senha = $_POST['senha'];
 	$email = $_POST['email'];
 	$curso = $_POST['curso'];
 	$instituicao = $_POST['instituicao'];
+	$lattes = $_POST['Lattes'];
 
-	$aluno = new Aluno($nome, $matricula, $email, $curso, $instituicao);
+	$aluno = new Aluno($nome, $matricula, $senha, $email, $curso, $instituicao, $lattes);
 
-	$mysql = new getConection();
+	$conection = new getConection();
+	$mysql = $conection->getMysql();
 
 	$daoAluno = new DaoALuno($mysql->getMysqli());
 
--- Model/Dados/ClassDaoAluno.php
+++ Model/Dados/ClassDaoAluno.php
@@ -12,48 +12,49 @@
 		}
 
 		public function insertAluno($aluno){
-			$query = "INSERT INTO aluno SET idAluno=NULL, $nomeAluno = ?, $matriculaAluno = ?, $emailAluno = ?, $cursoAluno = ?, $instituicaoAluno = ?";
+			$query = "INSERT INTO aluno SET idAluno=NULL, $nome=?, $matricula=?, $senha=?, $email=?, $curso=?, $instituicao=?, $lattes=?";
 			$stmt = $this->$mysqli->stmt_init();
 			$stmt->prepare($query);
-			$stmt->bind_param('sisss', $nomeAluno, $matriculaAluno, $emailAluno, $CursoAluno, $instituicaoAluno);
+			$stmt->bind_param('sssssss', $nomeAluno, $matriculaAluno, $senhaAluno, $emailAluno, $cursoAluno, $instituicaoAluno, $lattesAluno);
 			$nomeAluno = $aluno.getNome();
 			$matriculaAluno = $aluno->getMatricula();
+			$senhaAluno = $aluno->getSenha();
 			$emailAluno = $aluno->getEmail();
 			$CursoAluno = $aluno->getCurso();
 			$instituicaoAluno = $aluno->getInstituicao();
+			$lattesAluno = $aluno->getLattes();
 			$stmt->execute();
 			$stmt->close();
-			$this->$mysqli->close();
 		}
 
 		public function editAluno($aluno, $id){
-			$query = "UPDATE aluno SET $nomeAluno = ?, $matriculaAluno = ?, $emailAluno = ?, $cursoAluno = ?, $instituicao = ? WHERE idAluno=?";
+			$query = "UPDATE aluno SET $nome=?, $matricula=?, $senha=?, $email=?, $curso=?, $instituicao=?, $lattes=? WHERE idAluno=?";
 			$stmt = $this->$mysqli->stmt_init();
 			$stmt->prepare($query);
-			$stmt->bind_param('sissi', $nomeAluno, $matriculaAluno, $emailAluno, $CursoAluno, $instituicaoAluno, $idAluno);
-			$nomeAluno = $aluno->getNome();
+			$stmt->bind_param('sssssssi', $nomeAluno, $matriculaAluno, $senhaAluno, $emailAluno, $cursoAluno, $instituicaoAluno, $lattesAluno, $codAluno);
+			$nomeAluno = $aluno.getNome();
 			$matriculaAluno = $aluno->getMatricula();
+			$senhaAluno = $aluno->getSenha();
 			$emailAluno = $aluno->getEmail();
 			$CursoAluno = $aluno->getCurso();
 			$instituicaoAluno = $aluno->getInstituicao();
-			$idAluno = $id;
+			$lattesAluno = $aluno->getLattes();
+			$codAluno = $id;
 
 			$stmt->execute();
 			$stmt->close();
-			$this->$mysqli->close();
 		}
 
 		public function deleteAluno($id){
 			$query = "DELETE FROM aluno WHERE idAluno=?";
 			$stmt = $this->$mysqli->stmt_init();
 			$stmt->prepare($query);
-			$stmt->bind_param('i', $idAluno);
+			$stmt->bind_param('i', $codAluno);
 
-			$idAluno = $id;
+			$codAluno = $id;
 
 			$stmt->execute();
 			$stmt->close();
-			$this->$mysqli->close();
 		}
 
 	}
