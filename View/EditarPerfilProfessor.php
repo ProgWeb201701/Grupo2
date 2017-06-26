df28fb002d1dd81879eef0faf596e1300b09853c
Unknown <guilthys@gmail.com>
2017-06-26 20:43:51 -0300

    .

--- View/EditarPerfilProfessor.php
+++ View/EditarPerfilProfessor.php
@@ -27,7 +27,8 @@
 						<li class="dropdown">
 						<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Perfil <span class="caret"></span></a>
 							<ul class="dropdown-menu">
-								<li><a href="">Editar</a></li>
+								<li><a href="">Alterar Senha</a></li>
+								<li><a href="EditarPerfilProfessor.php">Editar</a></li>
 								<li><a href="">Remover</a></li>
 								</ul>
 						</li>
--- View/SubmeterTCC.php
+++ View/SubmeterTCC.php
@@ -27,8 +27,8 @@
 						<li class="dropdown">
 						<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Perfil <span class="caret"></span></a>
 							<ul class="dropdown-menu">
-							    <li><a href="EditarPerfilAluno.php">Editar</a></li>
-								<li><a href="">Editar</a></li>
+							    <li><a href="">Alterar Senha</a></li>
+							    <li><a href="EditarPerfilAluno.php">Editar</a></li>						
 								<li><a href="">Remover</a></li>
 								</ul>
 						</li>
