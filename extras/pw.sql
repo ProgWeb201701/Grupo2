c98c7bf5f7ce019f2261ce6c67421c578e73a267
Vitor Hugo Maciel dos Santos <vitao375@hotmail.com>
2017-07-01 17:41:23 -0300

    sql atualizado dnv

--- extras/pw.sql
+++ extras/pw.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: localhost
--- Data de Criação: 22-Jun-2017 às 19:48
+-- Data de Criação: 01-Jul-2017 às 17:40
 -- Versão do servidor: 5.5.28
 -- versão do PHP: 5.3.19
 
@@ -29,7 +29,7 @@ USE `pw`;
 --
 
 CREATE TABLE IF NOT EXISTS `aluno` (
-  `CodAluno` int(10) NOT NULL,
+  `CodAluno` int(10) NOT NULL AUTO_INCREMENT,
   `Nome` varchar(50) DEFAULT NULL,
   `Matricula` varchar(50) DEFAULT NULL,
   `Senha` varchar(30) DEFAULT NULL,
@@ -39,20 +39,15 @@ CREATE TABLE IF NOT EXISTS `aluno` (
   `LinkLattes` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`CodAluno`),
   KEY `Matricula` (`Matricula`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 --
--- Estrutura da tabela `atribuicao`
+-- Extraindo dados da tabela `aluno`
 --
 
-CREATE TABLE IF NOT EXISTS `atribuicao` (
-  `codFunc` int(10) DEFAULT NULL,
-  `idAval` int(10) DEFAULT NULL,
-  KEY `codFunc` (`codFunc`),
-  KEY `idAval` (`idAval`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `aluno` (`CodAluno`, `Nome`, `Matricula`, `Senha`, `email`, `Curso`, `instituicao`, `LinkLattes`) VALUES
+(10, 'gui', '123', '123', NULL, NULL, NULL, NULL),
+(11, 'vitor', '12345', '12345', 'vitao375@hotmail.com', '12345', '12345', NULL);
 
 -- --------------------------------------------------------
 
@@ -61,11 +56,17 @@ CREATE TABLE IF NOT EXISTS `atribuicao` (
 --
 
 CREATE TABLE IF NOT EXISTS `coordenadortcc` (
+  `idCordenador` int(10) NOT NULL AUTO_INCREMENT,
   `Nome` varchar(25) DEFAULT NULL,
-  `idCordenador` int(10) NOT NULL,
+  `siape` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `instituicao` varchar(100) NOT NULL,
+  `Area` varchar(50) NOT NULL,
+  `LinkLattes` varchar(200) NOT NULL,
   `Senha` varchar(30) DEFAULT NULL,
+  `Formacao` varchar(50) NOT NULL,
   PRIMARY KEY (`idCordenador`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -74,10 +75,19 @@ CREATE TABLE IF NOT EXISTS `coordenadortcc` (
 --
 
 CREATE TABLE IF NOT EXISTS `funcao` (
+  `codFunc` int(10) NOT NULL AUTO_INCREMENT,
   `NomFunc` varchar(50) DEFAULT NULL,
-  `codFunc` int(10) NOT NULL,
   PRIMARY KEY (`codFunc`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `funcao`
+--
+
+INSERT INTO `funcao` (`codFunc`, `NomFunc`) VALUES
+(1, 'CoordenadorDeTCC'),
+(2, 'Orientador'),
+(3, 'Avaliador');
 
 -- --------------------------------------------------------
 
@@ -106,12 +116,16 @@ CREATE TABLE IF NOT EXISTS `monografia` (
 --
 
 CREATE TABLE IF NOT EXISTS `nota` (
+  `codNota` int(10) NOT NULL AUTO_INCREMENT,
   `NotaMono` float DEFAULT NULL,
   `Arquivo` varchar(250) DEFAULT NULL,
   `Comentarios` varchar(250) DEFAULT NULL,
   `idAval` int(10) DEFAULT NULL,
-  KEY `idAval` (`idAval`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `codAluno` int(10) NOT NULL,
+  PRIMARY KEY (`codNota`),
+  KEY `idAval` (`idAval`),
+  KEY `codAluno` (`codAluno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -120,7 +134,7 @@ CREATE TABLE IF NOT EXISTS `nota` (
 --
 
 CREATE TABLE IF NOT EXISTS `professor` (
-  `idAval` int(10) NOT NULL,
+  `idAval` int(10) NOT NULL AUTO_INCREMENT,
   `Nome` varchar(50) DEFAULT NULL,
   `siape` varchar(20) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
@@ -129,8 +143,11 @@ CREATE TABLE IF NOT EXISTS `professor` (
   `LinkLattes` varchar(200) DEFAULT NULL,
   `Senha` varchar(50) DEFAULT NULL,
   `Formacao` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`idAval`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `codFunc` int(10) NOT NULL,
+  PRIMARY KEY (`idAval`),
+  KEY `codFunc` (`codFunc`),
+  KEY `codFunc_2` (`codFunc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -139,24 +156,17 @@ CREATE TABLE IF NOT EXISTS `professor` (
 --
 
 CREATE TABLE IF NOT EXISTS `turma` (
-  `idTurma` int(10) NOT NULL,
+  `idTurma` int(10) NOT NULL AUTO_INCREMENT,
   `Ano` varchar(10) DEFAULT NULL,
   `Curso` varchar(25) DEFAULT NULL,
   `idCordenador` int(10) DEFAULT NULL,
   PRIMARY KEY (`idTurma`),
   KEY `idCordenador` (`idCordenador`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Limitadores para a tabela `atribuicao`
---
-ALTER TABLE `atribuicao`
-  ADD CONSTRAINT `atribuicao_ibfk_1` FOREIGN KEY (`codFunc`) REFERENCES `funcao` (`codFunc`),
-  ADD CONSTRAINT `atribuicao_ibfk_2` FOREIGN KEY (`idAval`) REFERENCES `professor` (`idAval`);
 
 --
 -- Limitadores para a tabela `monografia`
@@ -169,7 +179,14 @@ ALTER TABLE `monografia`
 -- Limitadores para a tabela `nota`
 --
 ALTER TABLE `nota`
+  ADD CONSTRAINT `AlunoNota` FOREIGN KEY (`codAluno`) REFERENCES `aluno` (`CodAluno`),
   ADD CONSTRAINT `nota_ibfk_1` FOREIGN KEY (`idAval`) REFERENCES `professor` (`idAval`);
+
+--
+-- Limitadores para a tabela `professor`
+--
+ALTER TABLE `professor`
+  ADD CONSTRAINT `professor_ibfk_1` FOREIGN KEY (`codFunc`) REFERENCES `funcao` (`codFunc`);
 
 --
 -- Limitadores para a tabela `turma`
