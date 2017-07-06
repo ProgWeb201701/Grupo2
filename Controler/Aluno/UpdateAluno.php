900f55d82bd8719d92b2e6ec37502aa21f328dcc
Unknown <guilthys@gmail.com>
2017-07-06 03:22:39 -0300

    Edit quase 100%

--- Controler/Aluno/UpdateAluno.php
+++ Controler/Aluno/UpdateAluno.php
@@ -1,20 +1,32 @@
 <?php  
+session_start();
 include 'C:\WebServer\Apache2.2\htdocs\Grupo2\Model\ClassAluno.php';
 include  'C:\WebServer\Apache2.2\htdocs\Grupo2\Model\Dados\ClassConection.php';
 include 'C:\WebServer\Apache2.2\htdocs\Grupo2\Model\Dados\ClassDaoAluno.php';
     
 
-	$login = $_section["login"];
-	$senha = $_section["senha"];
+    $matricula = unserialize($_SESSION['user'])->getMatricula();
+    $senha = unserialize($_SESSION['user'])->getSenha();
+
+    $curso = $_POST['curso'];
+    $instituicao = $_POST['instituicao'];
+    $lattes = $_POST['lattes'];
+    $email = $_POST['email'];
+    $nome = $_POST['nome'];
+
+
+    $aluno = new Aluno($nome, $matricula, $senha, $email, $curso, $instituicao, $lattes);
+
 	$conection = new getConection();
 
 	$mysql = $conection->getMysql();
 
 	$daoAluno = new DaoALuno($conection->getMysql());
 
-	$aluno = new Aluno();
+	$daoAluno->updateAluno($aluno);
 
-	$aluno = $daoAluno->selectLoginAluno($login, $senha);
+	
+	
 
 	header("Location: ../View/EditarPerfilAluno.php");
 	exit();
